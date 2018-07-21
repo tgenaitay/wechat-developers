07fac15f96bcc907d83e777cedfc0f823c7536e3
thibault genaitay <tgenaitay@gmail.com>
2018-07-21 11:39:44 +0800

    build

--- config.rb
+++ config.rb
@@ -40,7 +40,13 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.deploy_method = :git
+end
+
